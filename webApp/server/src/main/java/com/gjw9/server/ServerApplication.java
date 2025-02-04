eb69a694dab790c35f871327833be80f6ea73329
Mehdi <surface@builgDiff.local>
2025-02-04 10:54:10 -0500

    debugging db connection with webapp server

--- webApp/server/src/main/java/com/gjw9/server/ServerApplication.java
+++ webApp/server/src/main/java/com/gjw9/server/ServerApplication.java
@@ -15,23 +15,24 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
-	@Bean
-	public Consumer<Message<String>> myConsumer(StreamBridge sb) {
-		return v -> {
-			//match received
-			System.out.println("Received myConsumer: " + v.getPayload());
-			System.out.println("CorrelationID: " + v.getHeaders().get("solace_correlationId"));
 
-			// object found
-			String myTopic = "object/found";
-			System.out.println("Publishing to: " + myTopic);
-			sb.send(myTopic, "object found");
+	// @Bean
+	// public Consumer<Message<String>> myConsumer(StreamBridge sb) {
+	// 	return v -> {
+	// 		//match received
+	// 		System.out.println("Received myConsumer: " + v.getPayload());
+	// 		System.out.println("CorrelationID: " + v.getHeaders().get("solace_correlationId"));
 
-			// object lost
-			myTopic = "object/lost";
-			System.out.println("Publishing to: " + myTopic);
-			sb.send(myTopic, "object lost");
-		};
-	}
+	// 		// object found
+	// 		String myTopic = "object/found";
+	// 		System.out.println("Publishing to: " + myTopic);
+	// 		sb.send(myTopic, "object found");
+
+	// 		// object lost
+	// 		myTopic = "object/lost";
+	// 		System.out.println("Publishing to: " + myTopic);
+	// 		sb.send(myTopic, "object lost");
+	// 	};
+	// }
 
 }
